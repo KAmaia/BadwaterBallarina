3b02fa026144cea1c3fc34f2bad1164b811951c9
Kamaia <krystalamaia@gmail.com>
2016-01-21 12:17:36 -0600

    removed erroneous directory

---
+++ BadwaterBallarina/Source/Configurator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BadwaterBallarina.Source {
+	class Configurator {
+		public Configurator( ) {
+
+		}
+		public IRCConfig ReadAndCreateConfig(string filePath ) {
+			IRCConfig ircConfig;
+
+			return ircConfig;
+		}
+	}
+}
---
+++ BadwaterBallarina/Source/IRCConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BadwaterBallarina.Source {
+	class IRCConfig {
+		#region fields
+		//Server Info
+		private string ircServer;
+		private int ircPort;
+
+		//User Info
+		private string ircUser;
+		private string ircNick;
+		private string ircPassw;
+
+		//channel info
+		private List<string> ircChannels;
+		#endregion
+
+		#region properties
+		//server info
+		public string IrcServer { get { return ircServer; } }
+		public int IrcPort { get { return ircPort; } }
+		
+		//User Info
+		public string IrcUser { get { return ircUser; } }
+		public string IrcNick { get { return ircNick; } }
+		public string IrcPassw { get { return ircPassw; } }
+
+		//channel info
+		public List<string> IrcChannels { get { return ircChannels; } }
+		#endregion
+
+	}
+}
