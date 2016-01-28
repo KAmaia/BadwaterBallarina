172fec2d7ab0bc03d5f94edfd118c34e86997718
Kamaia <krystalamaia@gmail.com>
2016-01-27 20:43:05 -0600

    Implemented Commands?

--- BadwaterBallarina/Source/IRC/Commands/HelloCommand.cs
+++ BadwaterBallarina/Source/IRC/Commands/HelloCommand.cs
@@ -15,6 +15,7 @@ namespace BadwaterBallarina.Source.IRC.Commands {
 
 		public void Execute( AIrcMessage message ) {
 			message.Respond( "Hello " + message.Sender );
+			//making a change for commit
 		}
 	}
 }
